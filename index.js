efae1241f3146adb13dba79acea2ea356e526e5f
M Newman <megan.m.boggs@gmail.com>
2022-11-01 11:51:00 -0700

    fix newCard function with conditionals

--- index.js
+++ index.js
@@ -50,10 +50,12 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  console.log(cards);
-  renderGame();
-  // console.log("Drawing a new card from the deck!")
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
+    // console.log("Drawing a new card from the deck!")
+  }
 }
