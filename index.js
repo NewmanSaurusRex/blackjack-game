2d1acb8f5731aaf6c6dc7ecf6383c51dd3904129
M Newman <megan.m.boggs@gmail.com>
2022-11-01 12:11:30 -0700

    add player object

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+  name: "M",
+  chips: 430,
+};
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -7,7 +11,8 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.getElementById("cards-el");
 
-console.log(cards);
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
   let randomNumber = Math.floor(Math.random() * 13) + 1;
